2280994c26e6770dfa49750e8397901ba97ac232
abdallah279 <abdallahzeko8@gmail.com>
2024-08-24 03:58:01 +0300

    contact and about-us and gallary pages res

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,172 +12,31 @@ window.addEventListener('scroll', function () {
 // SideBar
 $(".side-open").on("click", function () {
   $(".links").addClass("active");
-  $(".nav-overlay").addClass("active");
+  $(".overlay-m").fadeIn(500);
 });
 
 // SideBar
-$(".nav-overlay").on("click", function () {
+$(".overlay-m").on("click", function () {
   $(".links").removeClass("active");
-  $(this).removeClass("active");
+  $(".overlay-m").fadeOut(500);
 });
 
 let isRtl = $('html[lang="ar"]').length > 0;
 
-// Normal Select To
-if ($(".select").length > 0) {
-  $(".select").select2({
-    dir: isRtl ? "rtl" : "ltr",
-    minimumResultsForSearch: Infinity,
-    placeholder: function () {
-      $(this).data("placeholder");
-    },
-  });
-}
-
-//  CountUp
-$(".num").countUp();
-
-/************* Upload Files Or Img *************/
-let loginInputs = document.querySelectorAll(".img-upload-input");
-
-loginInputs.forEach((input) => {
-  if (input.classList.contains("profile")) {
-    let img = input.parentElement.querySelector(".profile-img .img");
-    let imageSrc = img.getAttribute("src");
-    input.onchange = function () {
-      let reader = new FileReader();
-      if (input.files[0]) {
-        reader.readAsDataURL(input.files[0]);
-      } else {
-        img.setAttribute("src", imageSrc);
-        img.classList.remove("wid");
-      }
-
-      reader.onload = () => {
-        img.setAttribute("src", reader.result);
-        img.classList.add("wid");
-      };
-    };
-  } else {
-    input.addEventListener("change", function () {
-      imgPreview(input);
-    });
+// Active Link
+$(".page-navbar .links .link").each(function () {
+  if (window.location.href.includes($(this).attr("href"))) {
+    $(this).addClass("active");
   }
 });
 
-
-// ImgPreview Function
-function imgPreview(input) {
-  var file = input.files[0];
-  var mixedfile = file["type"].split("/");
-  var filetype = mixedfile[0];
-  let photoContainer = $(input).closest(".upload-con").find(".photo-con");
-
-  const multiple = $(input).attr("multiple");
-
-  if (multiple) {
-    if (filetype == "image") {
-      uploadMultiImgs(input, photoContainer);
-    } else if (filetype == "application") {
-      uploadFile(input, photoContainer);
-    }
-  } else {
-    if (filetype == "image") {
-      uploadImg(input, photoContainer);
-    } else if (filetype == "application") {
-      photoContainer.empty();
-      uploadFile(input, photoContainer);
-    } else {
-      alert("Invalid file type");
-    }
-  }
+/******************* count up *******************/
+if ($(".num").length > 0) {
+  $(".num").countUp();
 }
 
-// uploadMultiImgs
-function uploadMultiImgs(input, photoContainer) {
-  for (file of input.files) {
-    let reader = new FileReader();
-    reader.onload = () => {
-      let img = `
-          <div class="hidden-img">
-              <input type='hidden' value='${reader.result}' />
-              <a class="fancybox" data-fancybox="gallery" href="${reader.result}">
-                  <img class="img" src="${reader.result}" />
-              </a>
-
-              <button type='button' class='remove-img'>
-                  <i class="fa-solid fa-xmark"></i>
-              </button>
-
-          </div>
-      `;
-
-      photoContainer.append(img);
-      removeIcon();
-    };
-    reader.readAsDataURL(file);
-  }
-}
-
-// Upload Image
-function uploadImg(input, photoContainer) {
-  var reader = new FileReader();
-  reader.onload = function (e) {
-    photoContainer.empty();
-    let img = `
-        <div class="hidden-img">
-
-            <a class="fancybox" data-fancybox="gallery" href="${e.target.result}">
-                <img class="img" src="${e.target.result}" />
-            </a>
-
-            <button type='button' class='remove-img'>
-                <i class="fa-solid fa-xmark"></i>
-            </button>
-
-        </div>
-    `;
-
-    photoContainer.append(img);
-    removeIcon();
-  };
-  reader.readAsDataURL(input.files[0]);
-}
-
-// uploadFiles
-function uploadFile(input, photoContainer) {
-  Object.values(input.files).forEach(function (file) {
-    var name = file.name;
-
-    let myFile = `
-          <div class="upload-label">
-              <input type='hidden' value='${name}' />
-              <img src='./assets/imgs/icons/pdf-file.gif' />
-              <span>${name}</span>
-              <button type='button' class='remove-img'>
-                <i class="fa-solid fa-xmark"></i>
-              </button>
-          </div>
-        `;
-
-    photoContainer.append(myFile);
-    removeIcon();
-  });
-}
-
-// Remove Icon
-function removeIcon() {
-  $(".remove-img").on("click", function (e) {
-    if (e.target.closest(".hidden-img")) {
-      e.target.closest(".hidden-img").remove();
-    } else if (e.target.closest(".upload-label")) {
-      e.target.closest(".upload-label").remove();
-    }
-  });
-}
-
-removeIcon();
-
+/******************* wow js *******************/
+new WOW().init();
 
 /******************* All Sliders *******************/
 $(document).ready(function () {
@@ -277,5 +136,21 @@ $(document).ready(function () {
       `<i class="fa-solid fa-arrow-left-long"></i>`,
     ],
   });
+});
+
+/************ faq Collapse ***********/
+$(document).ready(function () {
+  $('.faq-open').click(function (e) {
+    e.preventDefault()
+    if ($(this).parent('.faq-collapse').hasClass('active')) {
+      $(this).next(".faq-content").slideUp();
+      $(this).parent('.faq-collapse').removeClass('active');
+    } else {
+      $('.faq-collapse').removeClass('active');
+      $('.faq-content').slideUp();
+      $(this).parent('.faq-collapse').addClass('active');
+      $(this).next('.faq-content').slideDown();
+    }
+  })
 });
 

0a8df07d84f248da5fe11352f4bd8b1082ece3af
abdallah279 <abdallahzeko8@gmail.com>
2024-08-25 03:42:56 +0300

    blog 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,13 +30,53 @@ $(".page-navbar .links .link").each(function () {
   }
 });
 
-/******************* count up *******************/
+/******************* Select *******************/
+$(".select").select2({
+  dir: isRtl ? "rtl" : "ltr",
+  minimumResultsForSearch: Infinity,
+});
+
+/******************* Counter *******************/
 if ($(".num").length > 0) {
   $(".num").countUp();
 }
 
 /******************* wow js *******************/
 new WOW().init();
+
+/******************* About Tabs *******************/
+$('.tabs-show:not(:first)').hide();
+
+$('.tabs-m').on('click', function (e) {
+  e.preventDefault();
+
+  // FadeOut All Content
+  $('.tabs-show').fadeOut(0);
+  let item = $(this).attr('data-show');
+
+  // Show Current Content
+  $(`.${item}`).fadeIn();
+
+  // Active Class In Tabs
+  $('.tabs-m').removeClass('active');
+  $(this).addClass('active');
+
+});
+
+$('.about-open').click(function (e) {
+  e.preventDefault()
+  if ($(this).parent('.about-collapse').hasClass('active')) {
+    $(this).next(".about-content-tab").slideUp();
+    $(this).parent('.about-collapse').removeClass('active');
+    $(this).removeClass('active');
+  } else {
+    $('.about-collapse').removeClass('active');
+    $('.about-content-tab').slideUp();
+    $(this).parent('.about-collapse').addClass('active');
+    $(this).next('.about-content-tab').slideDown();
+    $(this).addClass('active');
+  }
+});
 
 /******************* All Sliders *******************/
 $(document).ready(function () {
@@ -74,6 +114,37 @@ $(document).ready(function () {
       `<i class="fa-solid fa-arrow-left-long"></i>`,
     ],
     responsive: {
+      991: {
+        items: 3,
+      },
+      560: {
+        items: 2,
+      },
+    },
+  });
+
+  /************ team Carousel ***********/
+  $(".team-slider").owlCarousel({
+    items: 1,
+    rtl: isRtl,
+    autoplaySpeed: 3000,
+    autoplayTimeout: 5000,
+    smartSpeed: 2000,
+    autoplayHoverPause: false,
+    margin: 25,
+    loop: false,
+    dots: false,
+    nav: true,
+    autoplay: true,
+    rewind: true,
+    navText: [
+      `<i class="fa-solid fa-arrow-right-long"></i>`,
+      `<i class="fa-solid fa-arrow-left-long"></i>`,
+    ],
+    responsive: {
+      1200: {
+        items: 4,
+      },
       991: {
         items: 3,
       },

d3d50672b9664a7b89b2e1c42574dac211da4483
abdallah279 <abdallahzeko8@gmail.com>
2024-09-21 01:22:54 +0300

    training-application

--- assets/js/main.js
+++ assets/js/main.js
@@ -225,3 +225,146 @@ $(document).ready(function () {
   })
 });
 
+
+
+
+/************* Upload Files Or Img *************/
+let loginInputs = document.querySelectorAll(".img-upload-input");
+
+loginInputs.forEach((input) => {
+  if (input.classList.contains("profile")) {
+    let img = input.parentElement.querySelector(".profile-img .img");
+    let imageSrc = img.getAttribute("src");
+    input.onchange = function () {
+      let reader = new FileReader();
+      if (input.files[0]) {
+        reader.readAsDataURL(input.files[0]);
+      } else {
+        img.setAttribute("src", imageSrc);
+        img.classList.remove("wid");
+      }
+
+      reader.onload = () => {
+        img.setAttribute("src", reader.result);
+        img.classList.add("wid");
+      };
+    };
+  } else {
+    input.addEventListener("change", function () {
+      imgPreview(input);
+    });
+  }
+});
+
+
+// ImgPreview Function
+function imgPreview(input) {
+  var file = input.files[0];
+  var mixedfile = file["type"].split("/");
+  var filetype = mixedfile[0];
+  let photoContainer = $(input).closest(".upload-con").find(".photo-con");
+
+  const multiple = $(input).attr("multiple");
+
+  if (multiple) {
+    if (filetype == "image") {
+      uploadMultiImgs(input, photoContainer);
+    } else if (filetype == "application") {
+      uploadFile(input, photoContainer);
+    }
+  } else {
+    if (filetype == "image") {
+      uploadImg(input, photoContainer);
+    } else if (filetype == "application") {
+      photoContainer.empty();
+      uploadFile(input, photoContainer);
+    } else {
+      alert("Invalid file type");
+    }
+  }
+}
+
+// uploadMultiImgs
+function uploadMultiImgs(input, photoContainer) {
+  for (file of input.files) {
+    let reader = new FileReader();
+    reader.onload = () => {
+      let img = `
+          <div class="hidden-img">
+              <input type='hidden' value='${reader.result}' />
+              <a class="fancybox" data-fancybox="gallery" href="${reader.result}">
+                  <img class="img" src="${reader.result}" />
+              </a>
+
+              <button type='button' class='remove-img'>
+                  <i class="fa-solid fa-xmark"></i>
+              </button>
+
+          </div>
+      `;
+
+      photoContainer.append(img);
+      removeIcon();
+    };
+    reader.readAsDataURL(file);
+  }
+}
+
+// Upload Image
+function uploadImg(input, photoContainer) {
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    photoContainer.empty();
+    let img = `
+        <div class="hidden-img">
+
+            <a class="fancybox" data-fancybox="gallery" href="${e.target.result}">
+                <img class="img" src="${e.target.result}" />
+            </a>
+
+            <button type='button' class='remove-img'>
+                <i class="fa-solid fa-xmark"></i>
+            </button>
+
+        </div>
+    `;
+
+    photoContainer.append(img);
+    removeIcon();
+  };
+  reader.readAsDataURL(input.files[0]);
+}
+
+// uploadFiles
+function uploadFile(input, photoContainer) {
+  Object.values(input.files).forEach(function (file) {
+    var name = file.name;
+
+    let myFile = `
+          <div class="upload-label">
+              <input type='hidden' value='${name}' />
+              <i class="fa-solid fa-file"></i>
+              <span>${name}</span>
+              <button type='button' class='remove-img'>
+                <i class="fa-solid fa-xmark"></i>
+              </button>
+          </div>
+        `;
+
+    photoContainer.append(myFile);
+    removeIcon();
+  });
+}
+
+// Remove Icon
+function removeIcon() {
+  $(".remove-img").on("click", function (e) {
+    if (e.target.closest(".hidden-img")) {
+      e.target.closest(".hidden-img").remove();
+    } else if (e.target.closest(".upload-label")) {
+      e.target.closest(".upload-label").remove();
+    }
+  });
+}
+
+removeIcon();
